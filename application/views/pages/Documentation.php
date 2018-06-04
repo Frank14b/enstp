35af68e11915f978284cce6e70a0c47fea4a4eda
Frank14b <franckfontcha@gmail.com>
2018-06-04 23:14:21 +0200

    commit

---
+++ application/views/pages/Documentation.php
@@ -0,0 +1,34 @@
+<!-- ********** Hero Area Start ********** -->
+<div class="hero-area">
+
+<!-- ********** Hero Area Start ********** -->
+<div class="hero-area height-400 bg-img background-overlay" style="background: url(<?=base_url()?>assets/img/student-education.jpg) center/cover; background-attachment:fixed"></div>
+    <!-- ********** Hero Area End ********** -->
+    <div class="container">
+        <div class="row" style="min-height:350px; margin-top:-165px;">
+                <div class="col-12 col-md-6 col-lg-4">
+                    <!-- Single Blog Post -->
+                    <div class="single-blog-post">
+                        <!-- Post Thumbnail -->
+                        <div class="post-thumbnail">
+                            <img src="<?=base_url()?>assets/img/core-img/Fotolia_55608174_S.jpg" alt="">
+                            <!-- Catagory -->
+                            <div class="post-cta"><a href="#">Froum</a></div>
+                        </div>
+                        <!-- Post Content -->
+                        <div class="post-content">
+                            <a href="#" class="headline">
+                                <h5>Vous avez et vous rencontrez des problemes lors de vos differents traitements dexercices et autres?</h5>
+                            </a>
+                            <p>Grace au forum VITERK retrouvez toute la communautes estudiantine de l'ENSTP et exposez y vos differents problemes pour pouvoir en debatre et vous apporter des solutions rapides et partager !</p>
+                            <!-- Post Meta -->
+                            <div class="post-meta">
+                                <p><a href="#" class="post-author">VIRTEK</a></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+        </div>
+    </div>
+</div>
+</div>
